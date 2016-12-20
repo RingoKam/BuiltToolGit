6a18319adc348f78a7b487cb7c2bc8ca8695b1a0
RingoKam <ringo_kam@hotmail.com>
2016-12-19 23:02:37 -0500

    Getting Git info now!

--- src/script.js
+++ src/script.js
@@ -1,3 +1,36 @@
-exports.scripts = () => {
-    console.log("Hello world?!?")
-}; 
+const fs = require('fs');
+const path = require('path');
+const getRepoInfo = require('git-repo-info');
+
+let gitFolder = [];
+
+console.log("Current Directory: " + __dirname); 
+
+function readDirectory(fileLocation) {
+    fs.readdir(__dirname, (err, files) => {
+        if (err) {
+            window.alert("Ops, Something bad happened: " + err);
+        } else {
+            for (let i = 0; i < files.length; i++) {
+                let fullpath = fileLocation + "/" + files[i];
+                if (IsFolder(files[i]) && IsGit(fullpath)) {
+                    gitFolder.push(files[i]); 
+                } else if(IsFolder(files[i])) {
+                    readDirectory(fullpath);
+                }
+            }
+        }
+    });
+}
+
+function IsFolder(fileName) {
+    if (!fileName.includes("."))
+        return true;
+}
+
+function IsGit(fullpath){
+    if(getRepoInfo(fullpath))
+        return true; 
+}
+
+console.log(gitFolder);
