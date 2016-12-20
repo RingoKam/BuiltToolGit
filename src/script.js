4f28269a6f9bbdf0edadb0b1ab0676e3200b0293
RingoKam <ringo_kam@hotmail.com>
2016-12-20 00:48:46 -0500

    fix bugged, now checking directory

--- src/script.js
+++ src/script.js
@@ -5,34 +5,44 @@ const getRepoInfo = require('git-repo-info');
 let gitFolder = [];
 
 
-let directory = "C:\Git";
+let directory = "C:\\Git\\reference_proj";
 console.log("Current Directory: " + directory);
 
 readDirectory(directory);
 
 function readDirectory(directory) {
-    fs.readdir(directory, (err, files) => {
-        if (err) {
-            //window.alert("Ops, Something bad happened: " + err);
-        } else {
-            //console.log("I am going in this files:");
-            //console.log(files);
-            for (let i = 0; i < files.length; i++) {
-                let fullpath = directory + "/" + files[i];
-                if (IsFolder(files[i]) && IsGit(fullpath)) {
-                    console.log(getRepoInfo(fullpath));
-                    gitFolder.push(files[i]);
-                } else if (IsFolder(files[i])) {
-                    readDirectory(fullpath);
+    if (fs.lstatSync(directory).isDirectory()) {
+        fs.readdir(directory, (err, files) => {
+            if (err) {
+                console.log("Ops, Something bad happened: " + err);
+            } else {
+                //console.log("I am going in this files:");
+                //console.log(files);
+                for (let i = 0; i < files.length; i++) {
+                    let fullpath = directory + "/" + files[i];
+                    if (IsFolder(files[i], fullpath) && IsGit(fullpath)) {
+                        let repoInfo = getRepoInfo(fullpath);
+                        gitFolder.push({
+                            "file": files[i],
+                            "repoInfo": repoInfo
+                        });
+                    } else if (IsFolder(files[i])) {
+                        readDirectory(fullpath);
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
 
-function IsFolder(fileName) {
-    if (!fileName.includes("."))
-        return true;
+function IsFolder(fileName, fullpath) {
+    try {
+        // if (fullpath && !fileName.includes(".") && !fs.lstatSync(fullpath).isDirectory())
+        if (!fileName.includes("."))
+            return true;
+    } catch (error) {
+        console.log(fileName, fullpath, error);
+    }
 }
 
 function IsGit(fullpath) {
