9b2af1b822f7ff0dbef4a03c620464308e9b69b6
RingoKam <ringo_kam@hotmail.com>
2016-12-20 22:14:56 -0500

    Fixed Async issue. read thru file sync... - refactor pending

--- src/git_folder_info.js
+++ src/git_folder_info.js
@@ -3,28 +3,23 @@ const path = require('path');
 const getRepoInfo = require('git-repo-info');
 
 function readDirectory(directory, gitFolder) {
-    if (fs.lstatSync(directory).isDirectory()) {
-        fs.readdir(directory, (err, files) => {
-            if (err) {
-                console.log("Ops, Something bad happened: " + err);
+    const isDirectory = fs.lstatSync(directory).isDirectory();
+    if (isDirectory) {
+        var files = fs.readdirSync(directory);
+        for (let i = 0; i < files.length; i++) {
+            let fullpath = directory + "/" + files[i];
+            if (IsGit(fullpath)) {
+                let repoInfo = getRepoInfo(fullpath);
+                gitFolder.push({
+                    "file": files[i],
+                    "repoInfo": repoInfo
+                });
             } else {
-                for (let i = 0; i < files.length; i++) {
-                    let fullpath = directory + "/" + files[i];
-                    if (IsGit(fullpath)) {
-                        let repoInfo = getRepoInfo(fullpath);
-                        gitFolder.push({
-                            "file": files[i],
-                            "repoInfo": repoInfo
-                        });
-                    } else {
-                        readDirectory(fullpath, gitFolder);
-                    }
-                }
+                readDirectory(fullpath, gitFolder);
             }
-        });
+        }
     }
-    return gitFolder; 
-}
+};
 
 function IsGit(fullpath) {
     if (getRepoInfo(fullpath).sha)
@@ -33,5 +28,6 @@ function IsGit(fullpath) {
 
 exports.GitFolders = (filePath) => {
     let gitFolder = [];
-    return readDirectory(filePath, gitFolder); 
+    readDirectory(filePath, gitFolder);
+    return gitFolder;
 } 
